3c8357575ea8c59f163fe9152071da137d1780bb
Adrián Molina Elvira <adrimo100@gmail.com>
2021-12-14 20:04:45 +0000

    uploading my code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#btn").addEventListener("Click", () => {
+    document.querySelector("#the-excuse").innerHTML = generatedExcuse();
+  });
+};
+
+let generatedExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let a, b, c, d;
+
+  a = Math.floor(Math.random() * who.length);
+
+  b = Math.floor(Math.random() * who.length);
+
+  c = Math.floor(Math.random() * who.length);
+
+  d = Math.floor(Math.random() * who.length);
+
+  var str = who[a] + " " + action[b] + " " + what[c] + " " + when[d];
+
+  return str;
 };
